dfc1184c6fb3dbd9ae19bb48eb17bfad5c50c87d
josephlimetauoe <j.k.l.limeta@sms.ed.ac.uk>
2023-06-07 15:27:08 +0400

    Converted variable units, changed categorical variable types, added starting visualizations
    + Changed weight variable to reflect the actual values
    + Converted number-encoded categorical variables to factors
    + Partially resolves #4 - Added a human-readable kable table and created a starter box plot visualization

--- r_scripts/analysis.R
+++ r_scripts/analysis.R
@@ -15,4 +15,30 @@ glimpse(mtcars_raw)
 # Summary statistics
 summary(mtcars_raw)
 
-# To do - data processing
+# Convert the weight variable to actual values in lbs and rename the variable to be more descriptive
+mtcars_proc <- mtcars_raw %>%
+  mutate(wt = wt * 1000) %>% 
+  rename(wt_lbs = wt)
+
+# Change number-encoded categorical variables to factors
+mtcars_proc$cyl <- as.factor(mtcars_proc$cyl)
+mtcars_proc$gear <- as.factor(mtcars_proc$gear)
+mtcars_proc$vs <- as.factor(mtcars_proc$vs)
+mtcars_proc$carb <- as.factor(mtcars_proc$carb)
+
+# Display the table in a human-readable format
+kable(mtcars_proc)
+
+# Create and display a box plot showing the mileage distribution for each engine cylinder configuration
+mpg_cyl_boxplot <- ggplot(mtcars_proc, aes(x=cyl, y=mpg, fill=cyl)) +
+  geom_boxplot() +
+  labs(
+    title = "Miles per Gallon Across Different Cylinder Configurations",
+    x = "No. of Cylinders",
+    y = "Miles per Gallon"
+  )
+
+mpg_cyl_boxplot
+
+# To do - Additional data processing and visualizations
+
