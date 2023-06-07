85fb7df01e6ca8c4f8ca22e72497490207119586
B209510 <107554366+B209510@users.noreply.github.com>
2023-06-07 18:50:50 +0100

    Add files via upload

--- example_r_file.R
+++ example_r_file.R
@@ -6,4 +6,26 @@ library(tidyverse)
 library(ggplot2)
 
 
-mtcars %>% head(1000000)
+mtcars %>% head(10)
+
+## adding some graphs and 
+# Summary statistics
+
+mtcars %>% 
+  summary()
+
+# draw a scatter plot of mpg vs. wt
+ggplot(mtcars, aes(wt, mpg)) +
+  geom_point() +
+  geom_smooth()
+
+#create factor cyl and am
+mtcars <- mtcars %>% 
+  mutate(fcyl = as.factor(cyl),
+         fam = as.factor(am))
+
+# Using mtcars, plot mpg vs. wt, colored by fcyl
+
+ggplot(mtcars, aes(wt, mpg, color = fcyl)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE)
